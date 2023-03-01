e1ca34511875be0053dcd2a724f2a771c51e7b71
Wayne Tucker <95285849+waynetuckermedia@users.noreply.github.com>
2023-03-01 15:40:11 -0500

    fixed constructors problem with string

--- src/Car.java
+++ src/Car.java
@@ -1,5 +1,11 @@
 public class Car {
 
+    public Car(int speed, double regularPrice, String color) {
+        this.speed = speed;
+        this.regularPrice = regularPrice;
+        this.color = color;
+    }
+
 
     // instance variables
     int speed;
--- src/MyOwnAutoShop.java
+++ src/MyOwnAutoShop.java
@@ -1,37 +1,20 @@
 public class MyOwnAutoShop {
     public static void main(String[] args) {
+
         // Create an instance of the Sedan class and initialize all the fields with appropriate values
-        Sedan sedan = new Sedan();
-        sedan.speed = 100;
-        sedan.regularPrice = 20000;
-        sedan.color = "Red";
-        sedan.length = 25;
+        Sedan sedan = new Sedan(22, 20000, "blue", 25);
 
         // Create two instances of the Ford class and initialize all the fields with appropriate values
-        Ford ford1 = new Ford();
-        ford1.speed = 120;
-        ford1.regularPrice = 30000;
-        ford1.color = "Blue";
-        ford1.year = 2022;
-        ford1.manufacturerDiscount = 5000;
-
-        Ford ford2 = new Ford();
-        ford2.speed = 110;
-        ford2.regularPrice = 25000;
-        ford2.color = "Black";
-        ford2.year = 2021;
-        ford2.manufacturerDiscount = 3000;
+        Ford ford1 = new Ford(2015, 1000, 15000, "red", 40);
+        Ford ford2 = new Ford(2017, 2000, 20000, "green", 45);
 
         // Create an instance of the Car class and initialize all the fields with appropriate values
-        Car car = new Car();
-        car.speed = 80;
-        car.regularPrice = 15000;
-        car.color = "Green";
+        Car car = new Car(60, 10000, "yellow");
 
         // Display the sale prices of all instances
         System.out.println("Sale price of Sedan: " + sedan.getSalePrice());
-        System.out.println("Sale price of Ford 1: " + ford1.getSalePrice());
-        System.out.println("Sale price of Ford 2: " + ford2.getSalePrice());
+        System.out.println("Sale price of Ford1: " + ford1.getSalePrice());
+        System.out.println("Sale price of Ford2: " + ford2.getSalePrice());
         System.out.println("Sale price of Car: " + car.getSalePrice());
     }
 }
